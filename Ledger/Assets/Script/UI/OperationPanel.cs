456d8ea0165ae4fcad94bc8685b01ab70f3d87f0
_FanXin <2799930626@qq.com>
2026-08-28 17:57:32 +0800

    upload new unity project

--- Ledger/Assets/Script/UI/OperationPanel.cs
+++ Ledger/Assets/Script/UI/OperationPanel.cs
@@ -1,18 +1,54 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class OperationPanel : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject transactionListContent;
+    public GameObject operationContent_Up;
+    public GameObject operationContent_Down;
     void Start()
     {
-        
+        transactionListContent = transform.parent.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ClickMask();
+    }
+
+    public void JudgeUpOrDown(RectTransform parentRectTransform)
+    {
+        float middleHeight = parentRectTransform.rect.height / 2f;
+    }
+
+    public void ClickMask()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            PointerEventData pointerData = new PointerEventData(EventSystem.current);
+            pointerData.position = Input.mousePosition;
+
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(pointerData, results);
+
+            if (results.Count > 0)
+            {
+                GameObject clickedGameObject = results[0].gameObject;
+
+                // 是否是自己或者自己的子物体(ISChildOf)
+                bool isSelfOrChild = ((clickedGameObject == gameObject) || clickedGameObject.transform.IsChildOf(transform));
+
+                if (isSelfOrChild)
+                {
+                    return;
+                }
+                else
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+        }
     }
 }
